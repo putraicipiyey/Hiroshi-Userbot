fb2556a3f075f571c1f30cb99cdaf6aa01f3d3be
BotName <90719292+UserbotMaps@users.noreply.github.com>
2022-01-24 15:28:13 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -33,9 +33,9 @@ async def help(event):
         for i in CMD_HELP:
             string += "`" + str(i)
             string += "`\t ✗  "
-        await event.edit("**✨ҡʏʏ-υѕєявσт✨**\n\n"
+        await event.edit("**✨ʜɪʀᴏsʜɪ-ᴜsᴇʀʙᴏᴛ✨**\n\n"
                          f"**◉ Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◉ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
                          "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
-                         f"◉ {string}◉\n\n✐ **ɴᴏᴛᴇꜱ :**  `.help animasi`\n☞  sᴜᴘᴘᴏʀᴛ : @NastySupportt")
+                         f"◉ {string}◉\n\n✐ **ɴᴏᴛᴇꜱ :**  `.help animasi`\n☞  sᴜᴘᴘᴏʀᴛ : @bombleebas")
         await asyncio.sleep(1000)
         await event.delete()
