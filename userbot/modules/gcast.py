8e795c86192bd3fff12593c6b06ed3e496e1c09c
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-02-25 08:50:36 +0700

    Update gcast.py

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -13,6 +13,28 @@
 from userbot import CMD_HELP
 from userbot.events import register
 
+GCAST_BLACKLIST = [
+    -1001473548283,  # SharingUserbot
+    -1001433238829,  # TedeSupport
+    -1001476936696,  # AnosSupport
+    -1001327032795,  # UltroidSupport
+    -1001294181499,  # UserBotIndo
+    -1001419516987,  # VeezSupportGroup
+    -1001459812644,  # GeezSupportGroup
+    -1001296934585,  # X-PROJECT BOT
+    -1001481357570,  # UsergeOnTopic
+    -1001459701099,  # CatUserbotSupport
+    -1001109837870,  # TelegramBotIndonesia
+    -1001752592753,  # Skyzusupport
+    -1001380293847,  # Kyy
+    -1001752592753,  # GcSkyzu
+    -1001456135097,  # SpamBot
+    -1001462425381,  # JANGAN DI APUS
+    -1001649802697,  # JANGAN DI APUS
+    -1001551881481,  # JANGAN DI APUS
+    -1001512737035,  # JANGAN DI APUS
+]
+
 
 @register(outgoing=True, pattern=r"^\.gcast(?: |$)(.*)")
 async def gcast(event):
