a4f8b06a4d462aa73705652cb285167609562cfd
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-03-04 23:38:12 +0700

    Create start.py

---
+++ userbot/utils/start.py
@@ -0,0 +1,20 @@
+from telethon import Button
+
+from userbot import BOTLOG, BOTLOG_CHATID, LOGS, tgbot
+
+
+async def startupmessage():
+    """
+    Start up message in telegram logger group
+    """
+    try:
+        if BOTLOG:
+            await tgbot.send_file(
+                BOTLOG_CHATID,
+                "https://telegra.ph/file/040c6a31ae08e75059ccf.jpg",
+                caption="🔥 **Hiroshi Userbot Has Been Actived**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 8.0@master\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @bombleebas ",
+                buttons=[(Button.url("ꜱᴜᴘᴘᴏʀᴛ", "https://t.me/hiroshisupport"),)],
+            )
+    except Exception as e:
+        LOGS.error(e)
+        return None
