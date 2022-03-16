967eee2e0cb46195fdd808fd3a0254c8055ec65e
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-03-17 06:28:55 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -17,7 +17,6 @@ from userbot import (
     BOT_TOKEN,
     BOT_VER,
     LOGS,
-    ALIVE_NAME,
     bot, 
 )
 from userbot.modules import ALL_MODULES
@@ -33,7 +32,7 @@ for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
 LOGS.info(
-    f"Jika {ALIVE_NAME} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/hiroshisupport")
+    f"Jika {user.first_name} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/hiroshisupport")
 LOGS.info(
     f"🔥Hiroshi-Userbot🔥 ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
 
