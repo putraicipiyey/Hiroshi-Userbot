7a327f0558baac1e00dc78e425708e6169978c3c
Indomiee <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-03-21 06:09:18 +0700

    Update __help.py

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -1,6 +1,28 @@
-from userbot import BOT_USERNAME, CMD_HELP, bot
-from userbot.utils import edit_delete, edit_or_reply, hiro_cmd
+#    TeleBot - UserBot
+#    Copyright (C) 2020 TeleBot
 
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#    Recode by Fariz <Github.com/farizjs>
+#    From Flicks-Userbot
+#    <t.me/TheFlicksUserbot>
+
+
+from userbot import BOT_USERNAME, CMD_HELP, bot, user
+from userbot.utils import edit_delete, edit_or_reply, skyzu_cmd
+
+DEFAULTUSER = user.first_name
 CUSTOM_HELP_EMOJI = "✨"
 
 
@@ -11,17 +33,17 @@ async def cmd_list(event):
         if args in CMD_HELP:
             await edit_or_reply(
                 event,
-                f"**✘ Commands available in `{args}` ✘** \n\n"
+                 f"**✘ Commands available in `{args}` ✘** \n\n"
                 + str(CMD_HELP[args])
                 + "\n\n**Support : @IndomieProject**"
                 + "\n**Store : @IndomieStore**",
             )
         else:
-            await edit_delete(event, f"**Module** `{args}` **Tidak tersedia, NGETIK YANG BENER NGENTOT!**")
+            await edit_delete(event, f"**Module** `{args}` **Tidak tersedia!**")
     else:
         try:
             results = await bot.inline_query(  # pylint:disable=E0602
-                BOT_USERNAME, "@HiroshiUserbot"
+                BOT_USERNAME, "@IndomieUserbot"
             )
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
