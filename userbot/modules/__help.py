1d7b7e1e8757cc13c871130413f7f34ac6c48b06
Grey Project <95908917+grey423@users.noreply.github.com>
2022-03-21 19:52:01 -0700

    Update __help.py

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -1,21 +1,20 @@
+
 from userbot import BOT_USERNAME, CMD_HELP, bot
-from userbot.utils import hiro_cmd
+from userbot.utils import edit_or_reply, edit_delete, hiro_cmd
 
 user = bot.get_me()
 DEFAULTUSER = user.first_name
 CUSTOM_HELP_EMOJI = "✨"
 
 
-
-
-@hiro_cmd(pattern="help(?: |$)(.*)")
+@hiro_cmd(pattern="help ?(.*)")
 async def cmd_list(event):
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
-            await event.edit(f"**✘ Commands available in {args} ✘** \n\n" + str(CMD_HELP[args]) + "\n\n**© @HiroshiSupport**")
+            await edit_or_reply(event, f"**✘ Commands available in {args} ✘** \n\n" + str(CMD_HELP[args]) + "\n\n**☞ @bombleebas**")
         else:
-            await event.edit(f"**Module** `{args}` **Tidak tersedia!**")
+            await edit_delete(event, f"**Module** `{args}` **Tidak tersedia!**")
     else:
         try:
             results = await bot.inline_query(  # pylint:disable=E0602
@@ -26,6 +25,6 @@ async def cmd_list(event):
             )
             await event.delete()
         except BaseException:
-            await event.edit(
-                f"** Sepertinya obrolan atau bot ini tidak mendukung inline mode. Untuk Mengaktifkanya silahkan ketik .inlineon**"
-            )
+            await edit_delete(event,
+                              f"** Sepertinya obrolan atau bot ini tidak mendukung inline mode.**"
+                              )
