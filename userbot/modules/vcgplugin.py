c4e60604623c708d8583f9585da423b3ead5cccf
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-03-24 06:30:14 +0700

    Update vcgplugin.py

--- userbot/modules/vcgplugin.py
+++ userbot/modules/vcgplugin.py
@@ -20,7 +20,6 @@ from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
 from userbot import PLAY_PIC as fotoplay
 from userbot import QUEUE_PIC as ngantri
-from userbot import call_py
 from userbot.utils import bash, edit_delete, edit_or_reply, hiro_cmd
 from userbot.utils.chattitle import CHAT_TITLE
 from userbot.utils.queues.queues import (
